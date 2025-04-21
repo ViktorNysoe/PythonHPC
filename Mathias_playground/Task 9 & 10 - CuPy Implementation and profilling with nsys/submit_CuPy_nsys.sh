c21b3b0a2e3aa961befadab4171cb9856d0858fb
Mathias_linux <mathiashl@pm.me>
2025-04-21 14:51:16 +0200

    Task 9 & 10

---
+++ Mathias_playground/Task 9 & 10 - CuPy Implementation and profilling with nsys/submit_CuPy_nsys.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#BSUB -J CuPy_Simulation
+#BSUB -q c02613
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+#BSUB -R "span[hosts=1]" 
+### -- set walltime limit: hh:mm --
+#BSUB -W 00:30
+### -- specify that we need 1GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -gpu "num=1:mode=exclusive_process" 
+#BSUB -o CuPy_Simulation_%J.out
+#BSUB -e CuPy_Simulation_%J.err
+#BSUB -u s204696@dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+
+# Initialize conda enviroment
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+lscpu # Print info about CPU
+
+nsys profile -o cupy_simulation python simulate_cupy.py 20 # This will simulate 20 floorplans and take the average time it took to simulate using the time command
+
