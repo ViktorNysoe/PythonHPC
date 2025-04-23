0647eecd2d0410a18f12c84e40465c864ced0967
leona <s204688@dtu.dk>
2025-04-23 10:01:25 +0200

    Parallelized numba jit implementation for Q11

---
+++ Bash_scripts/Q11_parallel_jit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -J simulate
+#BSUB -q hpc
+#BSUB -W 10
+#BSUB -o simulate%J.out
+#BSUB -e simulate%J.err
+#BSUB -n 5
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "select[model == XeonGold6126]"
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python Q7.py 20
